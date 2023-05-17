cd22ede89b9c6de6f6a43b8e9b7a93da4c29bd32
Abhinav Belhekar <73335766+CoderAbhinav@users.noreply.github.com>
2023-05-17 11:35:14 +0530

    added documentation

--- StreamingBitCipher/stream_cipher_algorithm.h
+++ StreamingBitCipher/stream_cipher_algorithm.h
@@ -5,20 +5,49 @@
 #include "utils.h"
 #define bitVector std::vector<bool>
 
+/**
+ * @class StreamCipherAlgorithm
+ * @brief Represents a bit scrambling cipher algorithm for stream encryption and decryption.
+ */
 class StreamCipherAlgorithm{
-    std::string key;
-    std::vector<bool> key_in_bit_vector;
+    std::string key; /**< The key used for encryption and decryption. */
+    std::vector<bool> key_in_bit_vector; /**< The key represented as a bit vector. */
 public:
+    /**
+     * @brief Constructs a StreamCipherAlgorithm object with the specified key.
+     * @param key The key used for encryption and decryption.
+     */
     StreamCipherAlgorithm(std::string);
+
+    /**
+     * @brief Encrypts the given message using the bit scrambling cipher.
+     * @param msg The message to be encrypted.
+     * @return The encrypted cipher text.
+     */
     std::string encrypt(std::string);
+
+    /**
+     * @brief Decrypts the given cipher text using the bit scrambling cipher.
+     * @param cipher_text The cipher text to be decrypted.
+     * @return The decrypted message.
+     */
     std::string decrypt(std::string);
 };
 
+/**
+ * @brief Constructs a StreamCipherAlgorithm object with the specified key.
+ * @param key The key used for encryption and decryption.
+ */
 StreamCipherAlgorithm::StreamCipherAlgorithm(std::string key) {
     this->key = key;
     this->key_in_bit_vector = stringToBitVector(key);
 }
 
+/**
+ * @brief Encrypts the given message using the bit scrambling cipher.
+ * @param msg The message to be encrypted.
+ * @return The encrypted cipher text.
+ */
 std::string StreamCipherAlgorithm::encrypt(std::string msg) {
     // create bit vectors for each
     bitVector msg_in_bit_vector = stringToBitVector(msg);
@@ -55,6 +84,11 @@ std::string StreamCipherAlgorithm::encrypt(std::string msg) {
     return bitVectorToString(cipher_in_bit_vector);
 }
 
+/**
+ * @brief Decrypts the given cipher text using the bit scrambling cipher.
+ * @param cipher_text The cipher text to be decrypted.
+ * @return The decrypted message.
+ */
 std::string StreamCipherAlgorithm::decrypt(std::string cipher_text) {
     // create bit vectors for each
     bitVector msg_in_bit_vector = bitVector(cipher_text.size() * 8, 0);
